4c0926d1e1f24ba073a9a5ab475cbcaafcf5dbe1
limo0088 <165850852+limo0088@users.noreply.github.com>
2024-04-08 20:09:32 +0200

    Update script.js

--- script.js
+++ script.js
@@ -1,9 +1,11 @@
-function checkPalindrome() {
-            var inputText = document.getElementById('inputText').value.toLowerCase();
-            var cleanedText = inputText.replace(/[^a-z0-9]/g, '');
-            var reversedText = cleanedText.split('').reverse().join('');
-            if (cleanedText === reversedText) {
-                document.getElementById('result').innerHTML = '"' + inputText + '" is a palindrome.';
-            } else {
-                document.getElementById('result').innerHTML = '"' + inputText + '" is not a palindrome.';
-            }}
+document.getElementById("checkButton").addEventListener("click", function() {
+    var inputText = document.getElementById('inputText').value.toLowerCase();
+    var cleanedText = inputText.replace(/[^a-z0-9]/g, '');
+    var reversedText = cleanedText.split('').reverse().join('');
+
+    if (cleanedText === reversedText) {
+        document.getElementById('result').innerHTML = '"' + inputText + '" is a palindrome.';
+    } else {
+        document.getElementById('result').innerHTML = '"' + inputText + '" is not a palindrome.';
+    }
+});
